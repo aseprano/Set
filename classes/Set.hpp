dc45eae3453b049cb6d363c2be54c5c9c28db819
Antonello <seprano.antonio@gmail.com>
2014-08-10 13:29:33 +0200

    [second commit]

--- classes/Set.hpp
+++ classes/Set.hpp
@@ -13,6 +13,61 @@
 
 class Set {
 public:
+	using iterator=std::set<int>::iterator;
+	using const_iterator=std::set<int>::const_iterator;
+	
+	Set() {};
+	Set(std::initializer_list<int> l);
+	Set(const Set& s);
+	Set(Set&& s);
+	Set& operator=(const Set& s) noexcept;
+	Set& operator=(Set&& s) noexcept;
+	
+	bool operator==(const Set& s) const noexcept;
+	bool operator!=(const Set& s) const noexcept {return !(*this == s);};
+	
+	Set operator+(const int& value) const noexcept;
+	Set operator+(int&& value) const noexcept;
+	Set operator+(const Set& s) const noexcept;
+	Set operator+(Set&& s) const noexcept;
+	Set& operator+=(const int& value) noexcept;
+	Set& operator+=(int&& value) noexcept;
+	Set& operator+=(const Set& s) noexcept;
+	Set& operator+=(Set&& s) noexcept;
+	
+	Set operator-(const int& value) const noexcept;
+	Set operator-(const Set& s) const noexcept;
+	Set& operator-=(const int& value) noexcept;
+	Set& operator-=(const Set& s) noexcept;
+	
+	Set intersection(const Set& s) noexcept;
+	
+	iterator begin() const noexcept {return _set.begin();};
+	iterator end()   const noexcept {return _set.end();};
+	
+	size_t size() const noexcept {return _set.size();};
+	bool empty() const noexcept {return _set.empty();};
+	void clear() noexcept;
+	
+	bool contains(const int& v) const noexcept;
+	bool contains(const Set& s) const noexcept;
+	
+	std::pair<iterator,bool> insert(const int& n) noexcept;
+	std::pair<iterator,bool> insert(int&& n) noexcept;
+	void insert(const Set& s) noexcept;
+	void insert(Set&& s) noexcept;
+	
+	template<class InputIterator>
+	void insert(InputIterator first, InputIterator last) {
+		for (auto it = first; it != last; it++) {
+			_set.insert(*it);
+		}
+	};
+	
+	Set::iterator erase(Set::const_iterator position);
+	size_t erase(const int& value);
+	iterator erase(const_iterator first, const_iterator last);
+	size_t erase(const Set& s) noexcept;
 	
 private:
 	std::set<int> _set;
--- main.cpp
+++ main.cpp
@@ -7,10 +7,22 @@
 //
 
 #include <iostream>
+#include "Set.hpp"
 
 int main(int argc, const char * argv[]) {
 	// insert code here...
-	std::cout << "Hello, World!\n";
+	Set s1{1,2,3,5}, s2{1,2,4,5}, s3 = s1.intersection(s2);
+	
+	if (s3.empty()) {
+		std::cout << "s3 is empty" << std::endl;
+	}
+	else {
+		std::cout << "s3: " << std::endl;
+		for (const auto& v : s3) {
+			std::cout << v << std::endl;
+		}
+	}
+	
 	return 0;
 }
 
